a0caec6bbfb40365fb685d66d2177c886aba02f7
Ubuntu <ubuntu@ip-172-31-39-224.us-east-2.compute.internal>
2020-02-14 17:31:08 +0000

    added the sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
+    before_create :remember_token
     has_secure_password
+    
+    # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+  
+    def User.new_token
+     SecureRandom.urlsafe_base64
+    end
 end
