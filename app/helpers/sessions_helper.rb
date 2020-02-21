8de225c894ef71f2eafa1192358fea54d150f543
Roheem Olayemi <abdurroheem20@gmail.com>
2020-02-21 22:16:05 +0100

    Updated the sessions helpers

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,18 +3,6 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # Returns the current logged-in user (if any).
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  # Remembers a user in a persistent session.
-  def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
-
   # Returns the user corresponding to the remember token cookie.
   def current_user
     if (user_id = session[:user_id])
