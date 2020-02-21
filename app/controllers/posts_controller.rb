ec02169a768eb45b76f2fa91734ba68252a1052c
kingsley McSimon Ogbonna <kingsley.mcsimon@gmail.com>
2020-02-20 21:47:04 -0800

    Linter error fixed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,17 +2,17 @@ class PostsController < ApplicationController
   before_action :logged_in_user, only: %I[new create]
 
   def new
-  @post = Post.new
+    @post = Post.new
   end
 
   def create
-  @post = Post.new(post_params)
+    @post = Post.new(post_params)
 
-  respond_to do |format|
+    respond_to do |format|
       if @post.save
-  @post.user_id = current_user.id
-    format.html { redirect_to root_url, notice: 'Post was successfully created.' }
-      format.json { render :show, status: :created, location: @post }
+        @post.user_id = current_user.id
+        format.html { redirect_to root_url, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
@@ -33,5 +33,4 @@ class PostsController < ApplicationController
   def logged_in_user
     redirect_to login_path unless logged_in?
   end
-  
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,16 @@
 class SessionsController < ApplicationController
-  def new 
-end
+  def new; end
 
   def create
-  user = User.find_by(email: params[:session][:email].downcase)
-  if user&.authenticate(params[:session][:password])
-  # Log the user in and redirect to the user's show page.
-  log_in user
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+      log_in user
       redirect_to user
-  else
+    else
       flash.now[:danger] = 'Invalid email or password'
       render 'new'
-  end
+    end
   end
 
   def destroy
